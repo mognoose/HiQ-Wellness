00442a0fc8455279fd1e151503ef2670ca952352
Csaba Szilagyi <csaba.szilagyi@hiq.fi>
2026-01-12 16:04:22 +0200

    responsive design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/fonts', '@nuxt/icon', '@pinia/nuxt'],
